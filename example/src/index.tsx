6cb950a612f62a045acf74e28666d424c2b91265
jerry <jerrynwosu007@gmail.com>
2023-01-21 23:42:47 +0100

    added style file to component

--- example/src/index.tsx
+++ example/src/index.tsx
@@ -1,18 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { MyCounter } from 'react-shadow-component'
+import { BoxShadow } from 'react-shadow-component'
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 root.render(
   <React.StrictMode>
     <div>
-      <h2>Default counter</h2>
-      <MyCounter />
-    </div>
-    <hr />
-    <div>
-      <h2>Counter with predefined value</h2>
-      <MyCounter value={5} />
+      <BoxShadow style='shadow1'>
+        <div style={{ width: '400px', height: '400px' }}>Hello</div>
+      </BoxShadow>
     </div>
   </React.StrictMode>,
 )
