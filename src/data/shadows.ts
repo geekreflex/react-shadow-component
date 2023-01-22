a0c8973c8877ddcdcb7d5f428011b97214845e37
joshnwosu <joshuanwosu078@gmail.com>
2023-01-22 01:09:15 +0100

    add shadow3

--- src/data/shadows.ts
+++ src/data/shadows.ts
@@ -1,4 +1,5 @@
 export const shadows: Record<string, string> = {
   shadow1: 'rgba(0, 0, 0, 0.16) 0px 1px 4px',
   shadow2: 'rgba(149, 157, 165, 0.2) 0px 8px 24px',
+  shadow3: 'rgba(0, 2, 0, 0.50) 0px 20px 16px'
 }
