03d6d6689565dd882ee0ca3589bc3ee8bd87e594
jerry <jerrynwosu007@gmail.com>
2023-01-29 11:40:52 +0100

    update: fix tslib path error in vite

--- example/src/index.tsx
+++ example/src/index.tsx
@@ -2,12 +2,17 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BoxShadow } from 'react-shadow-component'
 
+const styles: any = {
+  with: '400px',
+  border: '1px solid red',
+}
+
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 root.render(
   <React.StrictMode>
     <div>
-      <BoxShadow style='shadow2'>
-        <div style={{ width: '400px', height: '400px', padding: '20px' }}>Hello</div>
+      <BoxShadow color='red'>
+        <div style={styles}>Hello</div>
       </BoxShadow>
     </div>
   </React.StrictMode>,
